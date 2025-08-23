6e940a38976e4ef0bdd5f8f0731152551ffdd566
Michael Bishop <cleverca22@gmail.com>
2025-08-23 02:40:57 -0300

    partial fix for including nixpkgs

--- initial-setup.nix
+++ initial-setup.nix
@@ -1,15 +1,23 @@
 { pkgs, config, modulesPath, ... }:
 
-{
+let
+  # upstream nixos/lib/make-ext4-fs.nix doesnt leave enough inodes
+  # resulting in an image that fails the first boot
+  # this creates enough inodes to fill the 8k table, and create a new 8k table
+  dummy = pkgs.runCommand "dummy" {} ''
+    mkdir $out
+    cd $out
+    touch {1..1900}
+  '';
+in {
   imports = [
     "${modulesPath}/installer/sd-card/sd-image.nix"
 
-    # TODO, upstream nixos/lib/make-ext4-fs.nix doesnt leave enough inodes
-    # resulting in an image that fails the first boot
     "${modulesPath}/installer/cd-dvd/channel.nix"
   ];
   boot = {
     postBootCommands = ''
+      # ${dummy}
       if ! [ -e /etc/nixos/hardware-configuration.nix ]; then
         mkdir -pv /etc/nixos/
         pushd /etc/nixos/
